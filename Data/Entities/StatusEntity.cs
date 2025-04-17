ae7d7a78561d48f2111c40d3dc121877db4fd5b5
Tim <tim..newton.larsson@outlook.com>
2025-04-17 10:10:22 +0200

    Initialized data and business layers. Created entity models and configured project structure. Installed required EF Core and Identity packages.

---
+++ Data/Entities/StatusEntity.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Data.Entities;
+
+[Index(nameof(StatusName), IsUnique = true)]   
+public class StatusEntity
+{
+    [Key]
+    public string Id { get; set; } = null!;
+    public string StatusName { get; set; } = null!;
+    public virtual ICollection<ProjectEntity> Projects { get; set; } = [];
+}
---
+++ Data/Entities/UserEntity.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+
+namespace Data.Entities;
+
+public class UserEntity : IdentityUser
+{
+    public string? FullName { get; set; }
+    public string? JobTitle { get; set; }
+    public virtual ICollection<ProjectEntity> Projects { get; set; } = [];
+}
