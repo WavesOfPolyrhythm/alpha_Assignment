da62944ec9d65572b0af463143987250f57be733
Tim <tim..newton.larsson@outlook.com>
2025-04-17 11:36:48 +0200

    Set up AlphaDbContext with local SQL database. Registered context in Program.cs and configured Identity. Ran migrations and created database tables including Identity tables. Also added a BaseRepository with CRUD operations.

---
+++ Data/Repositories/BaseRepository.cs
@@ -0,0 +1,91 @@
+﻿using Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using System.Linq.Expressions;
+
+namespace Data.Repositories;
+
+public abstract class BaseRepository<TEntity> where TEntity : class
+{
+    protected readonly AlphaDbContext _context;
+    protected readonly DbSet<TEntity> _table;
+
+    protected BaseRepository(AlphaDbContext context)
+    {
+        _context = context;
+        _table = _context.Set<TEntity>();
+    }
+
+    public virtual async Task<bool> AddAsync(TEntity entity)
+    {
+        if (entity == null)
+            return false;
+
+        try
+        {
+            _table.Add(entity);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.Message);
+            return false;
+        }
+    }
+
+    public virtual async Task<IEnumerable<TEntity>> GetAllAsync()
+    {
+        var entities = await _table.ToListAsync();
+        return entities;
+    }
+
+    public virtual async Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> findBy)
+    {
+        var entity = await _table.FirstOrDefaultAsync(findBy);
+        return entity ?? null!;
+    }
+
+    public virtual async Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> findBy)
+    {
+        var exists = await _table.AnyAsync(findBy);
+        return exists;
+    }
+
+    public virtual async Task<bool> UpdateAsync(TEntity entity)
+    {
+        if (entity == null)
+            return false;
+
+        try
+        {
+            _table.Update(entity);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.Message);
+            return false;
+        }
+    }
+
+    public virtual async Task<bool> DeleteAsync(TEntity entity)
+    {
+        if (entity == null)
+            return false;
+
+        try
+        {
+            _table.Remove(entity);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.Message);
+            return false;
+        }
+    }
+
+}
--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,16 +1,33 @@
+using Data.Contexts;
+using Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
-
-
-
-
-
-
-
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AlphaDbContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("LocalDB")));
+builder.Services.AddIdentity<UserEntity, IdentityRole>(x =>
+{
+    x.User.RequireUniqueEmail = true;
+    x.Password.RequiredLength = 8;
+}).AddEntityFrameworkStores<AlphaDbContext>().AddDefaultTokenProviders();
+builder.Services.ConfigureApplicationCookie(x =>
+{
+    x.LoginPath = "/account/login";
+    x.AccessDeniedPath = "/account/denied";
+    x.Cookie.HttpOnly = true;
+    x.Cookie.IsEssential = true;
+    x.Cookie.Expiration = TimeSpan.FromHours(1);
+    x.SlidingExpiration = true;
+});
 
 var app = builder.Build();
 app.UseHttpsRedirection();
 app.UseRouting();
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapStaticAssets();
 app.MapControllerRoute(
